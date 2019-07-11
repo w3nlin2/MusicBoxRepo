9d6899ace39d602b74e92fd18b1ce6e0692f2be6
w3nlin2 <52774438+w3nlin2@users.noreply.github.com>
2019-07-12 03:35:01 +0800

    Update HomePage.vue

--- HomePage.vue
+++ HomePage.vue
@@ -1,19 +1,24 @@
+
 <template>
     <div>
         <homepageheader></homepageheader>
+        <homepageindex1></homepageindex1>
     </div>
 </template>
 <script>
     import HomePageHeader from "./subComponents/HomePageHeader"
+    import HomePageIndex1 from "./subComponents/HomePageIndex1"
+    import HomePageIndex2 from "./subComponents/HomePageIndex2"
+    
     export default{   //导出默认对象
         data(){           //当前组件共享数据
             return{}     //数据
         },
         components:{
-            "homepageheader":HomePageHeader
+            "homepageheader":HomePageHeader,
+            "homepageindex2":HomePageIndex2
         }
     }
 </script>
 <style>
-
 </style>
