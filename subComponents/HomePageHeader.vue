8e443e14de7e78123c46a58fd3b9f30e9b3ab01a
w3nlin2 <52774438+w3nlin2@users.noreply.github.com>
2019-07-12 02:02:44 +0800

    Update HomePageHeader.vue

--- subComponents/HomePageHeader.vue
+++ subComponents/HomePageHeader.vue
@@ -1,6 +1,38 @@
+
 <template>
     <div>
-        唯一的根元素
+        <div id="homePageHeader">
+            <div class="moreBar"></div>
+            <h1 class="headerTitle">VMUSICJIN</h1>
+            <div class="search"></div>
+        </div>
+        <div id="homePageNav">
+            <div class="recmd"><span>推荐</span></div>
+            <div class="rank"><span>排行</span></div>
+            <div class="singer"><span>歌手</span></div>
+        </div>
+        <div class="homePageCarouselBacBox">
+            <div class="carousel">
+            
+            </div>
+        </div>
+        <div class="homePageSpaceFill20px"></div>
+        <div class="homePageRecmdSongList">推荐歌单</div>
+        <ul class="homePageRecmdSongList_item">
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+            <li><div></div><p>哈记得你的梦想吗?别让你的梦想只有想</p></li>
+        </ul>
+
     </div>
 </template>
 <script>
@@ -11,5 +43,94 @@
     }
 </script>
 <style>
+div#homePageHeader,div#homePageNav{
+    width:100%;
+    height:80px;
+    background-color:red;
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+}
+div#homePageHeader>.moreBar{
+    width:80px;
+    height:80px;
+    background-color:blue;
+}
+div#homePageHeader>.headerTitle{
+    width:238px;
+    height:80px;
+    margin:0;
+    padding:0;
+    line-height:80px;
+    color:white;
+}
+div#homePageHeader>.search{
+    width:80px;
+    height:80px;
+    background-color:blue;
+}
+div#homePageNav span{
+    color:white;
+    padding-bottom:5px;
+    border:2px solid white;
+    border-top-color:transparent;
+    border-left-color:transparent;
+    border-right-color:transparent;
+}
+div.homePageCarouselBacBox{
+    width:100%;
+    height:180px;
+    background:red;
+    position:relative;
+}
+div.homePageCarouselBacBox>.carousel{
+    position:absolute;
+    top:0;
+    left:5%;
+    width:90%;
+    height:200px;
+    background:blue;
+    border-radius:10px;
+}
 
+div.homePageSpaceFill20px{
+    width:100%;
+    height:20px;
+}
+div.homePageRecmdSongList{
+    width:100%;
+    height:40px;
+    line-height:40px;
+    font-weight:bold;
+    text-align:left;
+}
+
+ul.homePageRecmdSongList_item{
+    width:100%;
+    background:pink;
+    list-style:none;
+    display:flex;
+    justify-content:space-around;
+    flex-wrap:wrap;
+    margin:0;
+    padding:0;
+}
+ul.homePageRecmdSongList_item>li{
+    width:120px;
+    
+    background:purple;
+    margin:0;
+    padding:0;
+}
+ul.homePageRecmdSongList_item div{
+    width:120px;
+    height:120px;
+    background:orange;
+    border-radius:5px;
+}
+ul.homePageRecmdSongList_item p{
+    padding:0 10px;
+    font-size:8px;
+    text-align:left;
+}
 </style>
