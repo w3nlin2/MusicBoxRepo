e5d003fee63bbbbd7cfa2ee367a288872349f584
w3nlin2 <52774438+w3nlin2@users.noreply.github.com>
2019-07-15 02:10:39 +0800

    Update HomePageIndex1.vue

--- MusicBoxRepoVueCli/src/components/subComponents/HomePageIndex1.vue
+++ MusicBoxRepoVueCli/src/components/subComponents/HomePageIndex1.vue
@@ -2,10 +2,24 @@
 <template>
     <div>
         
-        <div class="homePageCarouselBacBox">
-            <div class="carousel">
-                <img src="./../../assets/homePageImages/carousel1.jpg" alt="">
+    
+<div class="container">
+    <h1 class="page-header d-none">Bootstrap</h1>
+    <div class="carousel">
+        <div class="window">
+            <div class="image_reel">
+                <a href="#"><img src="./../../assets/homePageImages/carousel1.jpg"></a>
+                <a href="#"><img src="./../../assets/homePageImages/carousel1.jpg"></a>
+                <a href="#"><img src="./../../assets/homePageImages/carousel1.jpg"></a>
             </div>
+        </div>
+    
+    <div class="paging">
+        <a href="#" rel="1">。</a>
+        <a href="#" rel="2">。</a>
+        <a href="#" rel="3">。</a>
+    </div>
+</div>
         </div>
         <div class="homePageSpaceFill20px"></div>
         <div class="homePageRecmdSongList">推荐歌单</div>
@@ -27,35 +41,63 @@
     </div>
 </template>
 <script>
-    
+    import "../../assets/js/jquery-1.9.1.min.js"
+    import "../../../node_modules/jquery"
+    import "../../../node_modules/bootstrap"
+    import "../../../node_modules/popper.js"
     export default{   //导出默认对象
         data(){           //当前组件共享数据
-            return{}     //数据
-        }
+            return{
+                play:null,
+                $active:{},
+            }     
+        },
+        methods:{
+
+             
+
+    rotateSwitch(){
+        this.play=setInterval(function(){
+            this.$active=$(".paging a.active").next();
+            if(this.$active.length===0){
+                this.$active=$(".paging a:first");
+            }
+            $(".paging a").removeClass("active");
+        this.$active.addClass("active");
+        $(".image_reel").animate({
+            left:-(this.$active.attr("rel")-1)*$(".window").width()
+        },500);
+        },2000)
+    },
+
+        },
+        mounted(){
+            var This=this;
+            $(".paging").show();
+            $(".paging a:first").addClass("active");
+            $(".image_reel").css({"width":$(".window").width()*$(".image_reel img").size()});
+            This.rotateSwitch();
+            $(".paging a").click(function(){
+            $active=$(This);
+            clearInterval(play);
+            $(".paging a").removeClass("active");
+            $active.addClass("active");
+            $(".image_reel").animate({
+            left:-($active.attr("rel")-1)*$(".window").width()
+            },500)
+            This.rotateSwitch();
+            return false;
+    });
+            $(".image_reel a").hover(function(){
+        clearInterval(This.play);
+    },function(){
+        This.rotateSwitch();
+    });
+
     }
+}
 </script>
 <style>
-div.homePageCarouselBacBox{
-    width:100%;
-    height:180px;
-    background:red;
-    position:relative;
-}
-div.homePageCarouselBacBox>.carousel{
-    position:absolute;
-    top:0;
-    left:5%;
-    width:90%;
-    height:200px;
-    background:blue;
-    border-radius:10px;
-    overflow: hidden;
-}
-div.homePageCarouselBacBox>.carousel>img{
-    width: 100%;
-    height:100%;
-    
-}
 div.homePageSpaceFill20px{
     width: 100%;
     height:20px;
@@ -101,4 +143,70 @@ ul.homePageRecmdSongList_item p{
     font-size:8px;
     text-align:left;
 }
+
+
+
+
+.container{
+            width: 100%;
+            height:180px;
+            margin:0 auto;
+            position:relative;
+            background: red;
+            
+        }
+        .carousel{
+            width: 100%;
+            height:110%;
+            overflow:hidden;
+            position:relative;
+            /*outline:2px solid #f00;*/
+            border-radius: 20px;
+            
+        }
+        .window{
+            width: 100%;
+            height:110%;
+            
+            
+        }
+        .image_reel{
+            width: 100%;
+            height:110%;
+            position:absolute;
+            left: 0px;
+            top:0;
+          
+            
+        }
+        
+        .image_reel img{
+            float:left;
+            width:33.33%;
+            height:110%;
+            display: flex;
+            
+            vertical-align: middle;
+        }
+
+
+        .paging{
+            position:absolute;
+            bottom:20px;
+            left:45%;
+            display: none;
+        }
+        .paging a{
+            text-decoration:none;
+            color:#fff;
+        }
+        .paging a:hover{
+            background: #920000;
+            font-weight:bold;
+        }
+        .paging a.active{
+            background: #920000;
+            font-weight: bold;
+        }
+        
 </style>
