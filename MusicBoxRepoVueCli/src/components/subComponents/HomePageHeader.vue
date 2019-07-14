1bad322e2452da3ce92178e324fc1d2374455a35
w3nlin2 <52774438+w3nlin2@users.noreply.github.com>
2019-07-15 02:09:52 +0800

    Update HomePageHeader.vue

--- MusicBoxRepoVueCli/src/components/subComponents/HomePageHeader.vue
+++ MusicBoxRepoVueCli/src/components/subComponents/HomePageHeader.vue
@@ -1,26 +1,50 @@
 <template>
-    <div>
+    <div id="headerBox">
         <div id="homePageHeader">
             <div class="moreBar"><img src="./../../assets/homePageImages/more.png"></div>
             <h1 class="headerTitle">VMUSICJIN</h1>
             <div class="search"><img src="./../../assets/homePageImages/search.png"></div>
         </div>
-        <div id="homePageNav">
-            <div class="recmd"><span>推荐</span></div>
-            <div class="rank"><span>排行</span></div>
-            <div class="singer"><span>歌手</span></div>
-        </div>
+        <ul id="homePageNav" >
+            <li  :class="{underlineCla:false}" @click="underlineChange" v-for="(elem,i) in homePageNavData" :key=i>{{elem}}</li>
+        </ul>
     </div>
 </template>
 <script>
+    import "../../assets/js/jquery-1.9.1.min.js"
+    import "../../../node_modules/jquery"
+    import "../../../node_modules/bootstrap"
+    import "../../../node_modules/popper.js"
     export default{   //导出默认对象
         data(){           //当前组件共享数据
-            return{}     //数据
+            return{
+                homePageNavData:["推荐","排行","歌手"],
+                underlineBtn:false,
+            } 
+        },
+        methods:{
+            underlineChange(e){
+                for(var i=0;i<e.target.parentNode.children.length;i++){
+                    e.target.parentNode.children[i].classList.remove("underlineCla");
+                }
+                e.target.classList.add("underlineCla");
+                this.$emit("navValue",e.target.innerHTML) 
+            }
+        },
+        mounted(){
+            $("#homePageNav>li").eq(0).addClass("underlineCla");
         }
     }
 </script>
 <style>
- div#homePageHeader,div #homePageNav{
+div#headerBox{
+    position:fixed;
+    top:0;
+    left:0;
+    width: 100%;
+    z-index: 999;
+}
+ div#homePageHeader{
     width:100%;
     height:60px;
     background-color:red;
@@ -55,13 +79,28 @@ div#homePageHeader>.search{
     height:60px;
     
 }
-div#homePageNav span{
+ul#homePageNav{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 0;
+    margin:0;
+    width: 100%;
+    height:60px;
+    background: red;
+}
+ul#homePageNav>li{
+    cursor:pointer;
     color:white;
-    padding-bottom:5px;
+    padding: 5px 0;
+    font-weight: bolder;
+    user-select: none;
+}
+.underlineCla{
     border:2px solid white;
-    border-top-color:transparent;
-    border-left-color:transparent;
-    border-right-color:transparent;
+    border-left:transparent;
+    border-top:transparent;
+    border-right:transparent;
 }
 </style>
 
