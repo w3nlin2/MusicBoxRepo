d4524054daf6253eeac4c793278944f3dec26fc8
w3nlin2 <52774438+w3nlin2@users.noreply.github.com>
2019-07-16 05:21:43 +0800

    Update HomePageIndex1.vue

--- MusicBoxRepoVueCli/src/components/subComponents/HomePageIndex1.vue
+++ MusicBoxRepoVueCli/src/components/subComponents/HomePageIndex1.vue
@@ -1,26 +1,29 @@
 
 <template>
     <div>
-        
-    
-<div class="container">
-    <h1 class="page-header d-none">Bootstrap</h1>
-    <div class="carousel">
-        <div class="window">
-            <div class="image_reel">
-                <a href="#"><img src="./../../assets/homePageImages/carousel1.jpg"></a>
-                <a href="#"><img src="./../../assets/homePageImages/carousel1.jpg"></a>
-                <a href="#"><img src="./../../assets/homePageImages/carousel1.jpg"></a>
+
+     <div class="container">
+    <div id="mycarousel" class="carousel slide" data-ride="carousel" ><!--carousel旋转木马--><!--data-ride让旋转木马开始走-->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="../../assets/homePageImages/carousel1.jpg">
+            </div>
+            <div class="item">
+                <img src="../../assets/homePageImages/dzq.jpg">
+            </div>
+            <div class="item">
+                <img src="https://dummyimage.com/600x300/222/fff&text=dummyimage.com+rocks!">
             </div>
         </div>
-    
-    <div class="paging">
-        <a href="#" rel="1">。</a>
-        <a href="#" rel="2">。</a>
-        <a href="#" rel="3">。</a>
+        
+        <ul class="carousel-indicators">
+            <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#mycarousel" data-slide-to="1"></li>
+            <li data-target="#mycarousel" data-slide-to="2"></li>
+        </ul>
+        
     </div>
 </div>
-        </div>
         <div class="homePageSpaceFill20px"></div>
         <div class="homePageRecmdSongList">推荐歌单</div>
         <ul class="homePageRecmdSongList_item">
@@ -42,62 +45,58 @@
 </template>
 <script>
     import "../../assets/js/jquery-1.9.1.min.js"
+    import "../../assets/bs/js/bootstrap.js"
+    import "../../assets/bs/css/bootstrap.css"
+
     import "../../../node_modules/jquery"
     import "../../../node_modules/bootstrap"
     import "../../../node_modules/popper.js"
+    
     export default{   //导出默认对象
         data(){           //当前组件共享数据
             return{
-                play:null,
-                $active:{},
+                imgItems:[{key1:"active",key2:require('../../assets/homePageImages/carousel1.jpg')},
+                {key1:"active",key2:require('../../assets/homePageImages/carousel1.jpg')},
+                ]
             }     
         },
         methods:{
 
              
 
-    rotateSwitch(){
-        this.play=setInterval(function(){
-            this.$active=$(".paging a.active").next();
-            if(this.$active.length===0){
-                this.$active=$(".paging a:first");
-            }
-            $(".paging a").removeClass("active");
-        this.$active.addClass("active");
-        $(".image_reel").animate({
-            left:-(this.$active.attr("rel")-1)*$(".window").width()
-        },500);
-        },2000)
-    },
+    
 
         },
-        mounted(){
-            var This=this;
-            $(".paging").show();
-            $(".paging a:first").addClass("active");
-            $(".image_reel").css({"width":$(".window").width()*$(".image_reel img").size()});
-            This.rotateSwitch();
-            $(".paging a").click(function(){
-            $active=$(This);
-            clearInterval(play);
-            $(".paging a").removeClass("active");
-            $active.addClass("active");
-            $(".image_reel").animate({
-            left:-($active.attr("rel")-1)*$(".window").width()
-            },500)
-            This.rotateSwitch();
-            return false;
-    });
-            $(".image_reel a").hover(function(){
-        clearInterval(This.play);
-    },function(){
-        This.rotateSwitch();
-    });
+        
+           
 
     }
-}
+
 </script>
 <style>
+div#mycarousel{
+    width: 100%!important;
+    height: 240px!important;
+    margin-bottom:-20px; 
+}
+div.carousel-inner{
+    width: 100%!important;
+    height: 100%!important;
+}
+div.item{
+    width: 100%!important;
+    height: 100%!important;
+}
+img{
+    width: 100%!important;
+    height: 100%!important;
+}
+div.carousel-inner{
+    border-radius: 5px;
+    overflow: hidden;
+}
+
+
 div.homePageSpaceFill20px{
     width: 100%;
     height:20px;
@@ -147,66 +146,5 @@ ul.homePageRecmdSongList_item p{
 
 
 
-.container{
-            width: 100%;
-            height:180px;
-            margin:0 auto;
-            position:relative;
-            background: red;
-            
-        }
-        .carousel{
-            width: 100%;
-            height:110%;
-            overflow:hidden;
-            position:relative;
-            /*outline:2px solid #f00;*/
-            border-radius: 20px;
-            
-        }
-        .window{
-            width: 100%;
-            height:110%;
-            
-            
-        }
-        .image_reel{
-            width: 100%;
-            height:110%;
-            position:absolute;
-            left: 0px;
-            top:0;
-          
-            
-        }
-        
-        .image_reel img{
-            float:left;
-            width:33.33%;
-            height:110%;
-            display: flex;
-            
-            vertical-align: middle;
-        }
 
-
-        .paging{
-            position:absolute;
-            bottom:20px;
-            left:45%;
-            display: none;
-        }
-        .paging a{
-            text-decoration:none;
-            color:#fff;
-        }
-        .paging a:hover{
-            background: #920000;
-            font-weight:bold;
-        }
-        .paging a.active{
-            background: #920000;
-            font-weight: bold;
-        }
-        
 </style>
